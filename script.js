ddf22b0a5e09706de18d39afae860545ad35cf48
kauamantega <167893166+kauamantega@users.noreply.github.com>
2024-06-14 15:14:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Thor",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: ". "
             },
             {
                 texto: "Hulk",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: "."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "qual deles",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Homem de Ferro",
+                afirmacao: "."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "capitão america",
+                afirmacao: "."
             }
         ]
     },
