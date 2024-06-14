61b4e2c7c0045525184e8a31b37c810319c6e1f2
kauamantega <167893166+kauamantega@users.noreply.github.com>
2024-06-14 15:11:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "qual deles?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Thor",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Hulk",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
